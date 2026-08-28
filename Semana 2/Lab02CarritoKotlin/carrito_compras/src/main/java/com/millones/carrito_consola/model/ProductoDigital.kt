fe7a2d740e73166e759c1d9f73dcf95aa93e1885
Daniel Millones <dmillonesz@gmail.com>
2026-08-28 08:59:51 -0500

    Agregar subclases ProductoFisico y ProductoDigital con logica de mostrarDetalle polimorfica

--- Semana 2/Lab02CarritoKotlin/carrito_compras/src/main/java/com/millones/carrito_consola/model/ProductoDigital.kt
+++ Semana 2/Lab02CarritoKotlin/carrito_compras/src/main/java/com/millones/carrito_consola/model/ProductoDigital.kt
@@ -1,4 +1,9 @@
 package com.millones.carrito_consola.model
 
-class ProductoDigital {
+class ProductoDigital(nombre: String, precio: Double, cantidad: Int, val correoEnvio: String)
+    : Producto(nombre, precio, cantidad) {
+
+    override fun mostrarDetalle(index: Int): String {
+        return super.mostrarDetalle(index) + " [Digital - Correo: $correoEnvio]"
+    }
 }
--- Semana 2/Lab02CarritoKotlin/carrito_compras/src/main/java/com/millones/carrito_consola/model/ProductoFisico.kt
+++ Semana 2/Lab02CarritoKotlin/carrito_compras/src/main/java/com/millones/carrito_consola/model/ProductoFisico.kt
@@ -1,4 +1,9 @@
 package com.millones.carrito_consola.model
 
-class ProductoFisico {
+class ProductoFisico(nombre: String, precio: Double, cantidad: Int, val peso: Double)
+    : Producto(nombre, precio, cantidad) {
+
+    override fun mostrarDetalle(index: Int): String {
+        return super.mostrarDetalle(index) + String.format(" [Físico - Peso: %.1f kg]", peso)
+    }
 }
