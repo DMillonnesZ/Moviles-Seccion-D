f795da94bf583fa97a7b146581242f67f0dfd96a
Daniel Millones <dmillonesz@gmail.com>
2026-08-28 08:54:15 -0500

    Implementar modelo Cliente con constructor secundario y validacion

--- Semana 2/Lab02CarritoKotlin/carrito_compras/src/main/java/com/millones/carrito_consola/model/Cliente.kt
+++ Semana 2/Lab02CarritoKotlin/carrito_compras/src/main/java/com/millones/carrito_consola/model/Cliente.kt
@@ -1,4 +1,13 @@
 package com.millones.carrito_consola.model
 
 class Cliente {
+    private var nombre: String
+
+    constructor(nombre: String) {
+        this.nombre = if (nombre.isBlank()) "Cliente sin registrar" else nombre
+    }
+
+    constructor() : this("Cliente sin registrar")
+
+    fun getNombre(): String = nombre
 }
