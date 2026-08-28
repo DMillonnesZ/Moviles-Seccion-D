9389af6725994adf65d3824d49a541352879cc7d
Daniel Millones <dmillonesz@gmail.com>
2026-08-27 19:14:25 -0500

    Proyecto inicial con menú del carrito

---
+++ Semana 2/Lab02CarritoKotlin/carrito_consola/src/main/java/com/millones/carrito_consola/Carrito.kt
@@ -0,0 +1,7 @@
+package com.millones.carrito_consola
+
+fun main() {
+    println("=========================================")
+    println("     CARRITO DE COMPRAS - TIENDA TECSUP    ")
+    println("=========================================")
+}
