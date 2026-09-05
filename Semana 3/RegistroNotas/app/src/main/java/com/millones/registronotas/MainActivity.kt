d2f864a39ca5491b4cf072ba12c401507f243fac
Daniel Millones <dmillonesz@gmail.com>
2026-09-04 21:45:29 -0500

    Creacion de la estructura inicial del proyecto

---
+++ Semana 3/RegistroNotas/app/src/main/java/com/millones/registronotas/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.millones.registronotas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.millones.registronotas.ui.theme.RegistronotasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RegistronotasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
