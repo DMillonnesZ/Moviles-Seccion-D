4f0c63910a3756c39ec24fea9b8f7e50e208de24
Daniel Alejandro <dmillonesz@gmail.com>
2026-09-23 07:46:15 -0500

    Style: actualizar paleta de colores y theme
    Se actualizan Color.kt y Theme.kt con la paleta morado/lavanda/naranja/plomo definida para el Portal Academico.

--- Semana 5/app/src/main/java/com/millones/navlab/ui/theme/Color.kt
+++ Semana 5/app/src/main/java/com/millones/navlab/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.millones.navlab.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val MoradoOscuro = Color(0xFF4A2E9C)
+val MoradoMedio = Color(0xFF6C3FD1)
+val LavandaClaro = Color(0xFFE4DBF7)
+val MoradoClarito = Color(0xFFD8CCF0)
+val Blanco = Color(0xFFFFFFFF)
+val PlomoClaro = Color(0xFFE8E8EC)
+val GrisTextoSecundario = Color(0xFF6E6E7A)
+val RojoClaro = Color(0xFFFADBD8)
+val RojoOscuro = Color(0xFFB71C1C)
+val NaranjaGradiente = Color(0xFFF2A93B)
